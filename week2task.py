65e13f71e17f573921875842eca35eacd72b614e
tingcangnirelamor <tingcang.nirelamor@gmail.com>
2026-02-06 09:44:33 +0800

    Add files via upload

---
+++ week2task.py
@@ -0,0 +1,16 @@
+fullname = "Nirel Amor P. Tingcang"
+age = 19
+height = 146
+currently_working = False
+
+print("Fullname: ", fullname)
+print(type(fullname))
+
+print("\nAge: ", age)
+print(type(age))
+
+print("\nHeight: ", height)
+print(type(height))
+
+print("\nCurrently working? ",currently_working)
+print(type(currently_working))
